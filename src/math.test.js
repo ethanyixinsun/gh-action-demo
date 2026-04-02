9c5cdd9d26a9d89bf700f5d6dd4d5cfe0e006489
yixinsun <syxcoco7@gmail.com>
2026-04-01 20:34:02 -0700

    fix test

--- src/math.test.js
+++ src/math.test.js
@@ -6,7 +6,7 @@ describe('add', () => {
     expect(add(2, 3)).toBe(5);
   });
   it('returns the sum of two numbers - case 2', () => {
-    expect(add(-1, -2)).toBe(3);
+    expect(add(-1, -2)).toBe(-3);
   });
 });
 
